67adb24f49d76bdbc8c571601f2c88bb326f17ee
Chase Hooper <Hoopchaser@gmail.com>
2020-03-13 09:31:09 -0600

    comp done and passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Base Class
+
+class Vehicle:
+    pass
+
+#Extends from Vehicle
+
+class FlightVehicle(Vehicle):
+    pass
+
+#Extends from Flight Vehicle
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+#Extends from Vehicle
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
